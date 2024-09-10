6aa459681bcd0e36e35acf3a2f93f05b75081752
Abed Fetrat <abedfetrat@gmail.com>
2024-09-10 09:57:57 +0200

    feat: test api connection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import {useEffect} from "react";
+
 function App() {
+  useEffect(() => {
+    const fetchApi = async () => {
+      const result = await fetch("api/Pages");
+      console.log(await result.text());
+    }
+    fetchApi();
+  });
+
   return (
-      <>
-        <h1 className="text-3xl">devpage</h1>
-        <button className="btn">Click me!</button>
-      </>
+    <>
+      <h1 className="text-3xl">devpage</h1>
+      <button className="btn">Click me!</button>
+    </>
   )
 }
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5051",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      }
+    }
+  }
 })
