97d40709ea827e27a7b259ed3f821e4a535002eb
Abed Fetrat <abedfetrat@gmail.com>
2024-09-10 08:56:25 +0200

    add tailwind and daisyui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return (<h1>devpage</h1>)
+  return (
+      <>
+        <h1 className="text-3xl">devpage</h1>
+        <button className="btn">Click me!</button>
+      </>
+  )
 }
 
 export default App
