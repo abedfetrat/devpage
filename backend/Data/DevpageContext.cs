c7f3a043dd49e553eed7dfb1ba1fc528540ca8b1
abedfetrat <abedfetrat@gmail.com>
2024-09-11 21:39:16 +0200

    feat(api): update db context to auto include properties

--- backend/Data/DevpageContext.cs
+++ backend/Data/DevpageContext.cs
@@ -6,4 +6,10 @@ namespace backend.Data;
 public class DevpageContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Page> Pages { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Page>().Navigation(p => p.ProfileDetails).AutoInclude();
+        modelBuilder.Entity<Page>().Navigation(p => p.Links).AutoInclude();
+    }
 }
