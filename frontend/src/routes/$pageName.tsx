957ccc8f0819ad8db1195faa8f7b61c5b57840da
abedfetrat <abedfetrat@gmail.com>
2024-09-11 20:05:33 +0200

    fix: undefined value errors

--- frontend/src/routes/$pageName.tsx
+++ frontend/src/routes/$pageName.tsx
@@ -41,29 +41,35 @@ function Page() {
   return (
     <main className="container max-w-lg mx-auto py-20 text-center">
       <section>
-        <div className="avatar">
-          <div className="w-24 rounded-full mb-6">
-            <img src={author.photoUrl} alt="" />
+        {author?.photoUrl &&
+          <div className="avatar">
+            <div className="w-24 rounded-full mb-6">
+              <img src={author.photoUrl} alt=""/>
+            </div>
           </div>
-        </div>
-        <h1 className="text-xl font-bold">{author.firstName + " " + author.lastName}</h1>
-        <div className="flex flex-col gap-2 justify-center mt-4 text-sm font-medium">
-          <a href={`tel:${author.phone}`}>{author.phone}</a>
-          <a href={`mailto:${author.email}`}>{author.email}</a>
-        </div>
-        <ul className="flex flex-col gap-4 mt-8">
-          {page.links.map((link, index) => (
-            <li key={index}>
-              <a
-                href={link.url}
-                target={"_blank"}
-                rel="noopener noreferrer"
-                className="btn btn-outline btn-block">
-                {link.name}
-              </a>
-            </li>
-          ))}
-        </ul>
+        }
+        {author?.firstName && author?.lastName &&
+          <h1 className="text-xl font-bold">{author.firstName + " " + author.lastName}</h1>}
+        {(author?.phone || author?.email) &&
+          <div className="flex flex-col gap-2 justify-center mt-4 text-sm font-medium">
+            <a href={`tel:${author.phone}`}>{author.phone}</a>
+            <a href={`mailto:${author.email}`}>{author.email}</a>
+          </div>
+        }
+        {page.links &&
+          <ul className="flex flex-col gap-4 mt-8">
+            {page.links.map((link, index) => (
+              <li key={index}>
+                <a
+                  href={link.url}
+                  target={"_blank"}
+                  rel="noopener noreferrer"
+                  className="btn btn-outline btn-block">
+                  {link.name}
+                </a>
+              </li>
+            ))}
+          </ul>}
       </section>
     </main>
   );
