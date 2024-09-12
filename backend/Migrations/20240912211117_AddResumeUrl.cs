843b16b8f1e05ee2aac8bcee4be9d321cc47efcc
abedfetrat <abedfetrat@gmail.com>
2024-09-12 23:12:51 +0200

    feat(api): add endpoint for updating resume url

---
+++ backend/Migrations/20240912211117_AddResumeUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddResumeUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ResumeUrl",
+                table: "Pages",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResumeUrl",
+                table: "Pages");
+        }
+    }
+}
--- backend/Models/Page.cs
+++ backend/Models/Page.cs
@@ -6,6 +6,7 @@ public class Page
 {
     [Key] public required string UniqueName { get; set; }
     public required string UserId { get; set; }
+    public string? ResumeUrl { get; set; }
     public ProfileDetails? ProfileDetails { get; set; }
     public List<Link>? Links { get; set; }
 }
